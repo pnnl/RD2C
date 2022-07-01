dc13dab479b7aee627f75d66855e8328356e06ad
Bornstein, Marc <marco.bornstein@pnnl.gov>
2022-07-01 08:34:50 -0700

    debug cluster issues

--- Personalized-FL-Framework/run.sh
+++ Personalized-FL-Framework/run.sh
@@ -3,11 +3,11 @@
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
 #SBATCH -A rd2c
-#SBATCH -p gpu
+#SBATCH -p dl
 #SBATCH -t 2
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=2
-# #SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 #SBATCH -J fed_test
 #SBATCH -o test.out
 #SBATCH -e error.log
