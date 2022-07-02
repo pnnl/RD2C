87eed3ecb9efc260a87598b4f1d8469440419e89
Bornstein, Marc <marco.bornstein@pnnl.gov>
2022-07-01 19:55:10 -0700

    debug gpu storage issue

--- Decentralized-FL-Framework/main.py
+++ Decentralized-FL-Framework/main.py
@@ -7,6 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# test these
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+os.environ['XLA_PYTHON_CLIENT_ALLOCATOR'] = 'platform'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
 def run(rank, size):
