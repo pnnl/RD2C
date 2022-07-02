bdf2452b08715969d4dc36a0402419bbf4379e15
Bornstein, Marc <marco.bornstein@pnnl.gov>
2022-07-02 12:02:47 -0700

    test tensorflow script

---
+++ run_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH -A rd2c
+#SBATCH -p dl
+#SBATCH -t 30
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --gpus-per-node=1
+#SBATCH -J test
+
+module purge
+module load python/3.7.0
+source /people/born669/venv_fed/bin/activate
+
+srun python tensorflow_test.py
