f9cb47687ba1bf0bbcb5ec9f5bcaa82db9eeb992
Bornstein, Marc <marco.bornstein@pnnl.gov>
2022-12-08 13:22:49 -0800

    update scripts

--- MIDDLE/run_middle.sh
+++ MIDDLE/run_middle.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 #SBATCH -A rd2c
 #SBATCH -p dl
-#SBATCH -t 50
+#SBATCH -t 1:30:00
 #SBATCH -N 1
 #SBATCH -n 10
-#SBATCH --gpus-per-node=0
 #SBATCH -J middle_ablation
 
 module purge
-module load python/3.7.0
-module load cuda/11.1
-module load gcc
-module load openmpi
+module load gcc/8.1.0
+module load openmpi/4.1.4
 source /people/born669/middle/bin/activate
 
-mpirun -n 10 python MIDDLE_Ablation.py --name MIDDLE-1 --randomSeed 482
+mpirun -n 10 python MIDDLE_Ablation.py --name MIDDLE-1 --randomSeed 482 --epochs 30
