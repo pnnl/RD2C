da2ff3ff9bd2b0a74f6c58c363e7ddcb4e55b17f
born669 <marco.bornstein@pnnl.gov>
2023-01-06 12:55:02 -0800

    16 worker FC

--- MIDDLE/middle_ablation5FC.sh
+++ MIDDLE/middle_ablation5FC.sh
@@ -3,7 +3,7 @@
 #SBATCH -p dl
 #SBATCH -t 3:30:00
 #SBATCH -N 1
-#SBATCH -n 8
+#SBATCH -n 16
 #SBATCH -J ma5
 
 module purge
@@ -12,11 +12,11 @@ module load openmpi/4.1.4
 module load cuda/11.4
 source /people/born669/middle/bin/activate
 
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.5 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6666666666666666 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.25 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.1 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.05 --graph_type fully-connected
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.5 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6666666666666666 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.25 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.1 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.05 --graph_type fully-connected
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0 --graph_type fully-connected
