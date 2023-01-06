7c23063fc4884a0f1afe610163837dd5334a3e86
born669 <marco.bornstein@pnnl.gov>
2023-01-06 09:55:30 -0800

    Update Batch Scripts

--- MIDDLE/middle_ablation5.sh
+++ MIDDLE/middle_ablation5.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH -A rd2c
 #SBATCH -p dl
-#SBATCH -t 2:00:00
+#SBATCH -t 4:00:00
 #SBATCH -N 1
-#SBATCH -n 8
+#SBATCH -n 16
 #SBATCH -J ma5
 
 module purge
@@ -12,11 +12,11 @@ module load openmpi/4.1.4
 module load cuda/11.4
 source /people/born669/middle/bin/activate
 
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.5
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6666666666666666
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.6
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.25
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.1
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0.05
-mpirun -n 8 python MIDDLE.py --name MIDDLE-5 --randomSeed 9182 --epochs 50 --L3 0
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.5
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.6666666666666666
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.6
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.25
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.1
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0.05
+mpirun -n 16 python MIDDLE.py --name MIDDLE-5 --randomSeed 91162 --epochs 50 --L3 0
