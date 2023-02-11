7fffaa9b3b5d744f757792bec99babf7ff931426
born669 <marco.bornstein@pnnl.gov>
2023-02-11 13:47:09 -0800

    Add FedAvg Scripts

---
+++ MIDDLE/fedavg_ablation8.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#SBATCH -A rd2c
+#SBATCH -p dl
+#SBATCH -t 4:00:00
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH -J ma1
+
+module purge
+module load gcc/8.1.0
+module load openmpi/4.1.4
+module load cuda/11.4
+source /people/born669/middle/bin/activate
+
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 4282 --epochs 50
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 42 --epochs 50
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 1132 --epochs 50
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 2382 --epochs 50
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 91162 --epochs 50
