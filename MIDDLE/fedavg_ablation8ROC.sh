b3ab19850c4cbe4476fb23fc0662afdcea919d63
born669 <marco.bornstein@pnnl.gov>
2023-02-11 13:49:44 -0800

    Add FedAvg Scripts

--- MIDDLE/fedavg_ablation8ROC.sh
+++ MIDDLE/fedavg_ablation8ROC.sh
@@ -3,7 +3,7 @@
 #SBATCH -p dl
 #SBATCH -t 3:30:00
 #SBATCH -N 1
-#SBATCH -n 16
+#SBATCH -n 8
 #SBATCH -J ma1
 
 module purge
@@ -12,8 +12,8 @@ module load openmpi/4.1.4
 module load cuda/11.4
 source /people/born669/middle/bin/activate
 
-mpirun -n 16 python FedAvg.py --name FedAvg-1 --randomSeed 4282 --epochs 50 --graph_type clique-ring
-mpirun -n 16 python FedAvg.py --name FedAvg-1 --randomSeed 42 --epochs 50 --graph_type clique-ring
-mpirun -n 16 python FedAvg.py --name FedAvg-1 --randomSeed 1132 --epochs 50 --graph_type clique-ring
-mpirun -n 16 python FedAvg.py --name FedAvg-1 --randomSeed 2382 --epochs 50 --graph_type clique-ring
-mpirun -n 16 python FedAvg.py --name FedAvg-1 --randomSeed 91162 --epochs 50 --graph_type clique-ring
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 4282 --epochs 50 --graph_type clique-ring
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 42 --epochs 50 --graph_type clique-ring
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 1132 --epochs 50 --graph_type clique-ring
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 2382 --epochs 50 --graph_type clique-ring
+mpirun -n 8 python FedAvg.py --name FedAvg-1 --randomSeed 91162 --epochs 50 --graph_type clique-ring
